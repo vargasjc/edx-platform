89ef4f06cae1a8ac59d9aa953bcf484a00f186d7
Chris Rodriguez <github@chrisrodriguez.me>
2016-02-05 09:30:34 -0500

    Video player: adding Dragabilly to closed captions

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1267,9 +1267,9 @@ main_vendor_js = base_vendor_js + [
     'js/vendor/jquery-ui.min.js',
     'js/vendor/jquery.qtip.min.js',
     'js/vendor/jquery.ba-bbq.min.js',
-    'js/vendor/afontgarde/modernizr.fontface-generatedcontent.js',
-    'js/vendor/afontgarde/afontgarde.js',
-    'js/vendor/afontgarde/edx-icons.js'
+    'pattern-library/js/modernizr.custom.js',
+    'pattern-library/js/afontgarde.js',
+    'pattern-library/js/edx-icons.js',
 ]
 
 # Common files used by both RequireJS code and non-RequireJS code
@@ -1644,7 +1644,8 @@ REQUIRE_JS_PATH_OVERRIDES = {
     'js/student_account/logistration_factory': 'js/student_account/logistration_factory.js',
     'js/student_profile/views/learner_profile_factory': 'js/student_profile/views/learner_profile_factory.js',
     'js/bookmarks/bookmarks_factory': 'js/bookmarks/bookmarks_factory.js',
-    'js/groups/views/cohorts_dashboard_factory': 'js/groups/views/cohorts_dashboard_factory.js'
+    'js/groups/views/cohorts_dashboard_factory': 'js/groups/views/cohorts_dashboard_factory.js',
+    'draggabilly.pkgd': 'js/vendor/draggabilly.pkgd.js'
 }
 ################################# CELERY ######################################
 
--- lms/static/lms/js/require-config.js
+++ lms/static/lms/js/require-config.js
@@ -73,6 +73,7 @@
             "URI": "js/vendor/URI.min",
             "string_utils": "js/src/string_utils",
             "utility": "js/src/utility",
+            "draggabilly": "js/vendor/draggabilly.pkgd",
 
             // Files needed by OVA
             "annotator": "js/vendor/ova/annotator-full",
@@ -199,6 +200,9 @@
             },
             "moment-with-locales": {
                 exports: "moment"
+            },
+            "draggabilly": {
+                exports: "Draggabilly"
             }
         }
     });
